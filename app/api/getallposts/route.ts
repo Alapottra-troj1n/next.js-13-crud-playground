2572f715885ea88805428824b685bd767d35956a
Alapottra <alapottratrojon@gmail.com>
2023-03-28 12:31:59 +0600

    test3

--- app/api/getallposts/route.ts
+++ app/api/getallposts/route.ts
@@ -4,9 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try{
     const posts = await prisma.post.findMany();
-   if(posts){
     return NextResponse.json({success: true, posts});
-   }
   }catch(err){
     return NextResponse.json({success: false, err});
   }
